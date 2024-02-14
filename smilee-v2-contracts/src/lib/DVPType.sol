a2dca424528916cba62ef39c656963a1ec375edd
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-02-14 13:32:54 +0100

    Delete scope directories

--- smilee-v2-contracts/src/lib/DVPType.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.15;
-
-library DVPType {
-    bool public constant VANILLA = true;
-    bool public constant IG = false;
-}
--- smilee-v2-contracts/src/lib/OptionStrategy.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.15;
-
-library OptionStrategy {
-    bool public constant CALL = true; // UP strategy
-    bool public constant PUT = false; // DOWN strategy
-}
